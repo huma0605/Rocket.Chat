8e5a408f21b5b7395496c16b43837a5bd66bd95f
Hsin Yen Chung <stevenchung@Hsins-MacBook-Pro.local>
2023-11-25 17:39:52 -0800

    update trans files

--- packages/web-ui-registration/src/components/RegisterTitle.tsx
+++ packages/web-ui-registration/src/components/RegisterTitle.tsx
@@ -1,5 +1,6 @@
 import { useSetting } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
+import { Trans } from 'react-i18next';
 
 export const RegisterTitle = (): ReactElement | null => {
 	const hideTitle = useSetting<boolean>('Layout_Login_Hide_Title');
@@ -10,7 +11,7 @@ export const RegisterTitle = (): ReactElement | null => {
 
 	return (
 		<span id='welcomeTitle'>
-			Start to Chat with CoProduction.AI
+			<Trans i18nKey='registration.component.welcome'>Start to Chat with CoProduction.AI</Trans>
 		</span>
 	);
 };
